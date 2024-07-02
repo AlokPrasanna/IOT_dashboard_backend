808977fe3aeb5ebe8717923c10967a85d97977cc
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-02 12:53:51 +0530

    Created Device Model

---
+++ src/api/v1/models/Device.js
@@ -0,0 +1,44 @@
+// -----------------------Third-party libraries and modules-----------------------
+const mongoose = require("mongoose");
+
+// ---------- Device Schema ----------
+const DeviceSchema = new mongoose.Schema({
+    id: {
+        type:String,
+        require:true,
+    },
+    name: {
+        type:String,
+        require:true,
+    },
+    group: {
+        type:String,
+        require:true,
+    },
+    onState: {
+        type:Boolean,
+        require:true,
+    },
+    activeState: {
+        type:Boolean,
+        require:true,
+    },
+    dateCreated: {
+        type: String,
+        require:true
+    },
+    timeCreated: {
+        type: String,
+        require:true
+    },
+    dateUpdated: {
+        type: String,
+        require:true
+    },
+    timeUpdated: {
+        type: String,
+        require:true
+    },
+},{ timestamps: true });
+
+module.exports = mongoose.model("Device" , DeviceSchema);
