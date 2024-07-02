61e9f6e3d438deae08feb101db7211c992a1d5fd
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-02 12:57:43 +0530

    Exported Device Model

--- src/api/v1/models/index.js
+++ src/api/v1/models/index.js
@@ -1,7 +1,9 @@
 // ----------------------- Imports -----------------------
 const UserModel = require("./User");
+const DeviceModel = require("./Device");
 
 // ----------------------- Exports -----------------------
 module.exports = {
   UserModel,
+  DeviceModel,
 };
