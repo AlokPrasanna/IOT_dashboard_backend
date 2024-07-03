51ea4cc63db3ff593eb9b6f911e3ec5dcbb054c8
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-03 13:39:12 +0530

    Modified Code

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ const corsOptions = {
         callback(new Error('Not allowed by CORS'));
       }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200 
 };
 app.use(cors(corsOptions));
